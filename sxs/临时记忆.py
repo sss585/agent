1ed63905a17c3bedf7c24d0921f12bf221fc6c5f
shimimasai <16374217+shimimasai@user.noreply.gitee.com>
2026-04-24 00:25:39 +0800

    4.24

---
+++ sxs/临时记忆.py
@@ -0,0 +1,63 @@
+from langchain_community.chat_models.tongyi import ChatTongyi
+from langchain_core.prompts import PromptTemplate, ChatPromptTemplate, MessagesPlaceholder
+from langchain_core.output_parsers import StrOutputParser
+from langchain_core.runnables.history import RunnableWithMessageHistory
+from langchain_core.chat_history import InMemoryChatMessageHistory
+from get_api import get_openai_key
+model = ChatTongyi(model="qwen3-max",api_key=get_openai_key())
+# prompt = PromptTemplate.from_template(
+#     "你需要根据会话历史回应用户问题。对话历史：{chat_history}，用户提问：{input}，请回答"
+# )
+prompt = ChatPromptTemplate.from_messages(
+    [
+        ("system", "你需要根据会话历史回应用户问题。对话历史："),
+        MessagesPlaceholder("chat_history"),
+        ("human", "不要回答：{input}")
+    ]
+)
+
+str_parser = StrOutputParser()
+
+
+def print_prompt(full_prompt):#输出提示词内容，会被自动转换
+    print("="*20, full_prompt.to_string(), "="*20)#重载了，只提取出message部分
+    return full_prompt
+
+
+base_chain = prompt | print_prompt | model | str_parser
+
+
+store = {}      # key就是session，value就是InMemoryChatMessageHistory类对象
+# 实现通过会话id获取InMemoryChatMessageHistory类对象
+def get_history(session_id):#根据id提取，无对象则新建
+    print(store)
+    if session_id not in store:
+        store[session_id] = InMemoryChatMessageHistory()
+
+    return store[session_id]
+
+# 创建一个新的链，对原有链增强功能：自动附加历史消息
+conversation_chain = RunnableWithMessageHistory(
+    base_chain,     # 被增强的原有chain
+    get_history,    # 通过会话id获取InMemoryChatMessageHistory类对象
+    input_messages_key="input",             # 表示用户输入在模板中的占位符
+    history_messages_key="chat_history"     # 表示历史记录在模板中的占位符
+)
+#session_config--- get_history---store---prompt---model
+
+if __name__ == '__main__':
+    # 固定格式，添加LangChain的配置，为当前程序配置所属的session_id
+    session_config = {
+        "configurable": {
+            "session_id": "user_001"#只配置会话id
+        }
+    }
+
+    res = conversation_chain.invoke({"input": "小明有2个猫"}, session_config)
+    print("第1次执行：", res)
+
+    res = conversation_chain.invoke({"input": "小刚有1只狗"}, session_config)
+    print("第2次执行：", res)
+
+    res = conversation_chain.invoke({"input": "总共有几个宠物"}, session_config)#传入历史记录的配置文件
+    print("第3次执行：", res)
